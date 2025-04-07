889b271290e78edf935ec66cfd9f8a76363b551d
23130240 <23130240@st.hcmuaf.edu.vn>
2025-04-07 19:12:57 +0700

    reAdd

--- load.js
+++ load.js
@@ -1,3 +1,15 @@
-fetch('https://compassup.com/api/1/sourcing/products/search/recommend')
-  .then((response) => response.json())
-  .then((data) => console.log(data))
+function loadDoc(){
+    var xhttp = new XMLHttpRequest()
+    // function này sẽ chạy mỗi khi readyState thay đổi
+    xhttp.onreadystatechange = function () {
+      // khi request thành công
+      if(this.readyState == 4 && this.status == 200) {
+        document.getElementById('demo').innerHTML = this.responseText
+        console.log('XMLHttpRequest GET', this.responseText )
+      }
+      xhttp.open('GET', 'https://httpbin.org/get', true)
+      xhttp.send()
+    }
+    loadDoc()
+  }
+  
